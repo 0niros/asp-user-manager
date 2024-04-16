c5dceab0f5c796a9a222ed67ae52c188b1277633
Southstem <1311765133@qq.com>
2024-04-16 22:19:22 +0800

    Commit.

--- asp-user-service/src/main/java/cn/com/oniros/user/manager/UserApplication.java
+++ asp-user-service/src/main/java/cn/com/oniros/user/manager/UserApplication.java
@@ -1,10 +1,21 @@
 package cn.com.oniros.user.manager;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * cn.com.oniros.user.manager.UserApplication
  *
  * @author Li Xiaoxu
  * 2024/4/14 17:18
  */
+@SpringBootApplication
+@EnableDubbo
+@MapperScan(basePackages = "cn.com.oniros.user.manager.dao")
 public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
 }
